1a26de2b57dc3abe66ef6efe69a5a01a8f7c94c5
mjamesruggiero <mjamesruggiero@gmail.com>
2015-07-02 21:53:24 -0700

    fix dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "http://mjamesruggiero.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.3.4"]
-                 [ring "1.4.0-RC1"]
-                 [cheshire "5.5.0"]
-                 ]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [cheshire "5.5.0"]]
   :main swanson.core
   :profiles {:uberjar {:aot :all}})
