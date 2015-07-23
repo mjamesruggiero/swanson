95deed4035a29f52d9084965f365ded71f5b3007
mjamesruggiero <mjamesruggiero@gmail.com>
2015-07-22 22:57:56 -0700

    handlers

--- src/swanson/routes/home.clj
+++ src/swanson/routes/home.clj
@@ -3,12 +3,13 @@
             [liberator.core :refer [defresource resource request-method-in]]
             [swanson.views.layout :as layout]))
 
-(defn home []
-  (layout/common [:h1 "Hello World!"]))
+(defresource home
+  :service-available? false
+  :handle-service-not-available "service is currently unavailable..."
+  :method-allowed? (request-method-in :get)
+  :handle-ok "Hello world!"
+  :etag "fixed-etag"
+  :available-media-types ["text-plain"])
 
 (defroutes home-routes
-  (ANY "/" request
-       (resource
-         :handle-ok "Hello world!"
-         :etag "fixed-etag"
-         :available-media-types ["text-plain"])))
+  (ANY "/" request home))
