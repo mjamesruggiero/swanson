aba135ee9e8e482f6dd16e4c88827c92afad7d08
mjamesruggiero <mjamesruggiero@gmail.com>
2015-07-25 20:50:26 -0700

    add resources

--- src/swanson/routes/home.clj
+++ src/swanson/routes/home.clj
@@ -4,9 +4,12 @@
             [swanson.views.layout :as layout]))
 
 (defresource home
-  :service-available? false
+  :service-available? true
   :handle-service-not-available "service is currently unavailable..."
   :method-allowed? (request-method-in :get)
+  :handle-method-not-allowed
+    (fn [context]
+      (str (get-in context [:request :request-method]) " is not allowed"))
   :handle-ok "Hello world!"
   :etag "fixed-etag"
   :available-media-types ["text-plain"])
