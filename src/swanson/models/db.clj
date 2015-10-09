03c7cd1743a11674ad68b763c865fe9191c3f6ef
mjamesruggiero <mjamesruggiero@gmail.com>
2015-10-08 22:19:04 -0700

    Make improvements to user model

--- src/swanson/models/db.clj
+++ src/swanson/models/db.clj
@@ -10,11 +10,24 @@
   (sql/with-connection db
     (sql/create-table
       :users
-      [:id "varchar(32) PRIMARY KEY"]
-      [:username "varchar(100)"]
-      [:pass "varchar(100)"])))
+      [:id "serial primary key"]
+      [:fname "varchar(100) NOT NULL"]
+      [:lname "varchar(100) NOT NULL"]
+      [:email "varchar(100) NOT NULL"]
+      [:encrypted_password "varchar(100)"])))
 
-(defn get-user "first query" [id]
-  (sql/with-connection db
+(defn get-user
+  [id]
+  (sql/with-connection
+    db
     (sql/with-query-results
-      res ["SELECT * FROM users WHERE id = ?"] (first res))))
+      res ["SELECT * FROM users WHERE id = ?" id] (first res))))
+
+(defn make-user
+  [fname lname email encrypted-pass]
+  (sql/with-connection
+    db
+    (sql/insert-values
+      :users
+      [:fname :lname :email :encrypted_password]
+      [fname lname email encrypted-pass])))
