8ba699a11082bad170fe5f664f955d678b8dde14
mjamesruggiero <mjamesruggiero@gmail.com>
2015-10-09 21:29:14 -0700

    Add additional transaction and category tables

--- src/swanson/models/db.clj
+++ src/swanson/models/db.clj
@@ -16,6 +16,23 @@
       [:email "varchar(100) NOT NULL"]
       [:encrypted_password "varchar(100)"])))
 
+(defn create-transactions-table []
+  (sql/with-connection db
+    (sql/create-table
+      :transactions
+      [:id "serial primary key"]
+      [:amount "float NOT NULL"]
+      [:date "date NOT NULL"]
+      [:category_id "int NOT NULL"]
+      [:description "varchar(512) NOT NULL"])))
+
+(defn create-categories-table []
+  (sql/with-connection db
+    (sql/create-table
+      :categories
+      [:id "serial primary key"]
+      [:name "varchar(512) NOT NULL"])))
+
 (defn get-user
   [id]
   (sql/with-connection
@@ -31,3 +48,35 @@
       :users
       [:fname :lname :email :encrypted_password]
       [fname lname email encrypted-pass])))
+
+(defn get-category
+  [name]
+  (sql/with-connection
+    db
+    (sql/with-query-results
+      res ["SELECT * FROM categories WHERE name = ?" name] (first res))))
+
+(defn make-category
+  [category]
+  (sql/with-connection
+    db
+    (sql/insert-values
+      :categories
+      [:name]
+      [category])))
+
+(defn get-transaction
+  [id]
+  (sql/with-connection
+    db
+    (sql/with-query-results
+      res ["SELECT * FROM transactions WHERE id = ?" id] (first res))))
+
+(defn make-transaction
+  [amt dt desc & category-id]
+  (sql/with-connection
+    db
+    (sql/insert-values
+      :transactions
+      [:amount :date :category_id :description]
+      [amt dt category-id desc])))
