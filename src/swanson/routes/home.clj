593228af19c1fd07206c4a5bddd7b63e42d61809
mjamesruggiero <mjamesruggiero@gmail.com>
2015-10-13 23:11:49 -0700

    Remove unused code

--- src/swanson/routes/home.clj
+++ src/swanson/routes/home.clj
@@ -1,48 +1,10 @@
 (ns swanson.routes.home
   (:require [compojure.core :refer :all]
-            [liberator.core :refer [defresource resource request-method-in]]
             [swanson.views.layout :as layout]
-            [cheshire.core :refer [generate-string]]
-            [noir.io :as io]
-            [clojure.java.io :refer [file]]))
+            [noir.session :as session]))
 
-(def users (atom ["John" "Jane"]))
-
-(defresource get-users
-  :allowed-methods [:get]
-  :handle-ok (fn [_] (generate-string @users))
-  :available-media-types ["application/json"])
-
-(defresource add-user
-  :method-allowed? (request-method-in :post)
-  :post!
-  (fn [context]
-    (let [params (get-in context [:request :form-params])]
-      (swap! users conj (get params "user"))))
-  :handle-created (fn [_] (generate-string @users))
-  :available-media-types ["application/json"])
-
-(defresource home
-  :available-media-types ["text-html"]
-  :exist?
-  (fn [context]
-    [(io/get-resource "/home.html")
-     {::file (file (str (io/resource-path) "/home.html"))}])
-  :handle-ok
-  (fn [{{{resource :resource} :route-params} :request}]
-    (clojure.java.io/input-stream (io/get-resource "/home.html")))
-  :last-modified
-  (fn [{{{resource :resource} :route-params} :requests}]
-    (.lastModified (file (str (io/resource-path) "/home.html")))))
-
-(defresource config [k]
-  :available-media-types ["application/json"]
-  :handle-ok
-  (fn [_]
-    (generate-string {:status "alive" :key k})))
+(defn home []
+  (layout/common [:h1 "Hello folks"]))
 
 (defroutes home-routes
-  (ANY "/" request home)
-  (ANY "/add-user" request add-user)
-  (ANY "/users" request get-users)
-  (ANY "/config/:k" [k] (config k)))
+  (ANY "/" [] (home)))
