089f21b15d04e5cb0e2a9e2d54259b1e85cde61c
mjamesruggiero <mjamesruggiero@gmail.com>
2015-10-15 21:04:20 -0700

    Can parse Wells Fargo CSV

--- src/swanson/utils.clj
+++ src/swanson/utils.clj
@@ -3,8 +3,20 @@
     [clojure.data.csv :as csv]
     [clojure.java.io :as io]))
 
+; TODO might be better to do this w/o filehandle hassles
+; a la http://stackoverflow.com/a/19656800
 (defn get-csv
   [filepath]
   (with-open [in-file (io/reader filepath)]
     (doall
       (csv/read-csv in-file))))
+
+(defn- row->map [row]
+  (let [[date amount _ _ description] row]
+    {:date date :amount amount :description description}))
+
+(defn parse-bank-csv
+  "parse csv into importable map"
+  [filepath]
+  (let [parsed-csv (get-csv filepath)]
+    (map #(row->map %) parsed-csv)))
--- test/swanson/utils_test.clj
+++ test/swanson/utils_test.clj
@@ -8,6 +8,12 @@
 (deftest get-csv-test
   (testing "Test basic CSV functionality"
     (let [csv (swanson.utils/get-csv filepath)]
-      (is (= (first csv) ["date" "total_spend"]))
-      (is (= (count csv) 3))
-      (is (= (last csv) ["2014-07-27" "730393.8492"])))))
+      (is (= (first csv) ["2014-07-26" "683734.5288" "fake description 1"]))
+      (is (= (count csv) 2))
+      (is (= (last csv) ["2014-07-27" "730393.8492" "fake description 2"])))))
+
+(deftest parse-bank-csv
+  (testing "test CSV to seq of maps functionality"
+    (let [csv (swanson.utils/parse-bank-csv filepath)]
+      (is (= (first csv) {:date "2014-07-26" :amount "683734.5288" :description "fake description 1"}))
+      (is (= (last csv) {:date "2014-07-27" :amount "730393.8492" :description "fake description 2"})))))
