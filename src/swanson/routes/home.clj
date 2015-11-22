70287866f29a75ce76da04a327df39f0e490103f
mjamesruggiero <mjamesruggiero@gmail.com>
2015-11-21 22:32:15 -0800

    change "Hello" to "swanson"

--- src/swanson/routes/home.clj
+++ src/swanson/routes/home.clj
@@ -4,7 +4,7 @@
             [noir.session :as session]))
 
 (defn home []
-  (layout/common [:h1 "Hello " (session/get :user)]))
+  (layout/common [:h1 "swanson " (session/get :user)]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
